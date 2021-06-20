2df63e6c73fddd0cec1e7a1c1479acfa0bef4354
pixelsuft‮ <68371847+Pixelsuft@users.noreply.github.com>
2021-06-20 09:51:54 +0700

    Nice! Part Files Support !!!!!!

--- mods.js
+++ mods.js
@@ -1,10 +1,10 @@
 zipMods = [
-	['halva_en.zip', 'Half-Life EN (214M)', 214283501],
-	['blue_shift_rus.zip', 'Blue Shift EN (221M)', 221191140],
-	['opposing_force_en.zip', 'Opposing Force EN (221M)', 221191140],
-	['halva_rus.zip', 'Half-Life RUS (304M)', 303881453],
-	['blue_shift_rus.zip', 'Blue Shift RUS (223M)', 223479923],
-	['opposing_force_rus.zip', 'Opposing Force RUS (224M)', 223479923]
+	['halva_en.zip', 'Half-Life EN (214M)', 214283501, 0],
+	['blue_shift_rus.zip', 'Blue Shift EN (221M)', 221191140, 0],
+	['opposing_force_en.zip', 'Opposing Force EN (221M)', 221191140, 0],
+	['halva_rus.zip', 'Half-Life RUS (304M)', 303881453, 12, 26214400],
+	['blue_shift_rus.zip', 'Blue Shift RUS (223M)', 223479923, 0],
+	['opposing_force_rus.zip', 'Opposing Force RUS (224M)', 223479923, 0]
 ];
 
 pkgMods = [
@@ -14,32 +14,32 @@ var iArgs=document.getElementById('iArgs');
 selectZip.addEventListener('change', function(){
 	if(selectZip.value=="halva_en.zip")
 	{
-		alert('Bugs: menu lagging.');
+		//alert('Bugs: menu lagging.');
 		iArgs.value="-dev 1 -game valve";
 	}
 	else if(selectZip.value=="blue_shift_en.zip")
 	{
-		alert('Bugs: Sometimes needs noclip, because you can\'t pick armor');
+		//alert('Bugs: Sometimes needs noclip, because you can\'t pick armor');
 		iArgs.value="-dev 1 -game valve";
 	}
 	else if(selectZip.value=="opposing_force_en.zip")
 	{
-		alert('Bugs: Menu lagging, doesn\'t spawn NPCs (because old xash version).');
+		//alert('Bugs: Menu lagging, doesn\'t spawn NPCs (because old xash version).');
 		iArgs.value="-dev 1 -game gearbox";
 	}
 	else if(selectZip.value=="halva_rus.zip")
 	{
-		alert('Bugs: No bugs.');
+		//alert('Bugs: No bugs.');
 		iArgs.value="-dev 1 -game valve";
 	}
 	else if(selectZip.value=="blue_shift_rus.zip")
 	{
-		alert('Bugs: Sometimes needs noclip, because you can\'t pick armor');
+		//alert('Bugs: Sometimes needs noclip, because you can\'t pick armor');
 		iArgs.value="-dev 1 -game valve";
 	}
 	else if(selectZip.value=="opposing_force_rus.zip")
 	{
-		alert('Bugs: Doesn\'t spawn NPCs (because old xash version).');
+		//alert('Bugs: Doesn\'t spawn NPCs (because old xash version).');
 		iArgs.value="-dev 1 -game valve";
 	}
 });
